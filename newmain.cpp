299cef7347fcdc02d6e32d3dc950f6ada59a27a2
baskooiker <baskooiker90@gmail.com>
2014-03-31 16:36:26 +0200

    -

--- newmain.cpp
+++ newmain.cpp
@@ -22,46 +22,58 @@
 
 using namespace std;
 
-void threadOne(void* arg) {
-    vector<string> filenames;
-    filenames.push_back("data/makifree_1.dat");
+void writeToFile(string s){
+    fstream os;
+    os.open("data/test.txt");
+    os << s.c_str() << "\n";
+    os.close();
+    printf((s + "\n").c_str());
+}
 
-    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-    sde.setOutputFilename("results/free1.csv");
-    sde.setTotalNrGest(20);
-    sde.evaluate();
+void threadOne(void* arg) {
+//    vector<string> filenames;
+//    filenames.push_back("data/makifree_1.dat");
+//
+//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+//    sde.setOutputFilename("results/free1.csv");
+//    sde.setTotalNrGest(4);
+//    sde.evaluate();
+    
+    writeToFile("this is thread 1");
 }
 
 void threadTwo(void* arg) {
-    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
-    //    gse->setFilename("data/gestureSet20.dat");
-    //    gse->setOutputFilename("gestureset20PairsOutput83.csv");
-    //    gse->setTotalNrGest(20);
-    //    gse->setSkip(3);
-    //    gse->evaluatePairs();
-    vector<string> filenames;
-    filenames.push_back("data/makifree_2.dat");
-
-    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-    sde.setOutputFilename("results/free2.csv");
-    sde.setTotalNrGest(4);
-    sde.evaluate();
+//    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
+//    //    gse->setFilename("data/gestureSet20.dat");
+//    //    gse->setOutputFilename("gestureset20PairsOutput83.csv");
+//    //    gse->setTotalNrGest(20);
+//    //    gse->setSkip(3);
+//    //    gse->evaluatePairs();
+//    vector<string> filenames;
+//    filenames.push_back("data/makifree_2.dat");
+//
+//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+//    sde.setOutputFilename("results/free2.csv");
+//    sde.setTotalNrGest(4);
+//    sde.evaluate();
+    writeToFile("this is thread 2");
 }
 
 void threadThree(void* arg) {
-    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
-    //    gse->setFilename("data/gestureSet20.dat");
-    //    gse->setOutputFilename("gestureset20PairsOutput85.csv");
-    //    gse->setTotalNrGest(20);
-    //    gse->setSkip(5);
-    //    gse->evaluatePairs();
-    vector<string> filenames;
-    filenames.push_back("data/makifree_3.dat");
-
-    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-    sde.setOutputFilename("results/free3.csv");
-    sde.setTotalNrGest(4);
-    sde.evaluate();
+//    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
+//    //    gse->setFilename("data/gestureSet20.dat");
+//    //    gse->setOutputFilename("gestureset20PairsOutput85.csv");
+//    //    gse->setTotalNrGest(20);
+//    //    gse->setSkip(5);
+//    //    gse->evaluatePairs();
+//    vector<string> filenames;
+//    filenames.push_back("data/makifree_3.dat");
+//
+//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+//    sde.setOutputFilename("results/free3.csv");
+//    sde.setTotalNrGest(4);
+//    sde.evaluate();
+    writeToFile("this is thread 1");
 }
 
 /*
@@ -87,12 +99,10 @@ int main(int argc, char** argv) {
     HANDLE t1 = (HANDLE) _beginthread(threadOne, 0, (void*) 1);
     HANDLE t2 = (HANDLE) _beginthread(threadTwo, 0, (void*) 3);
     HANDLE t3 = (HANDLE) _beginthread(threadThree, 0, (void*) 5);
-    //    HANDLE t4 = (HANDLE) _beginthread(threadFour, 0, (void*) 7);
 
     WaitForSingleObject(t1, INFINITE);
     WaitForSingleObject(t2, INFINITE);
     WaitForSingleObject(t3, INFINITE);
-    //    WaitForSingleObject(t4, INFINITE);
 
     return 0;
 }
