a26fd794af2c4c3c182b3682218979be3c4ae8dd
baskooiker <baskooiker90@gmail.com>
2014-04-01 11:46:44 +0200

    some changes

--- newmain.cpp
+++ newmain.cpp
@@ -19,61 +19,66 @@
 #include <fstream>
 #include <process.h>
 #include <windows.h>
+#include <stdlib.h>
 
 using namespace std;
 
-void writeToFile(string s){
-    fstream os;
-    os.open("data/test.txt");
+void writeToFile(string s) {
+    ofstream os;
+    do {
+//        Sleep(rand() % 100 + 100);
+        os.open("results/test.txt", ofstream::app | ofstream::out);
+        printf("opened for: %s\n", s.c_str());
+    } while (!os.is_open());
     os << s.c_str() << "\n";
     os.close();
     printf((s + "\n").c_str());
 }
 
 void threadOne(void* arg) {
-//    vector<string> filenames;
-//    filenames.push_back("data/makifree_1.dat");
-//
-//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-//    sde.setOutputFilename("results/free1.csv");
-//    sde.setTotalNrGest(4);
-//    sde.evaluate();
-    
+    //    vector<string> filenames;
+    //    filenames.push_back("data/makifree_1.dat");
+    //
+    //    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+    //    sde.setOutputFilename("results/free1.csv");
+    //    sde.setTotalNrGest(4);
+    //    sde.evaluate();
+
     writeToFile("this is thread 1");
 }
 
 void threadTwo(void* arg) {
-//    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
-//    //    gse->setFilename("data/gestureSet20.dat");
-//    //    gse->setOutputFilename("gestureset20PairsOutput83.csv");
-//    //    gse->setTotalNrGest(20);
-//    //    gse->setSkip(3);
-//    //    gse->evaluatePairs();
-//    vector<string> filenames;
-//    filenames.push_back("data/makifree_2.dat");
-//
-//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-//    sde.setOutputFilename("results/free2.csv");
-//    sde.setTotalNrGest(4);
-//    sde.evaluate();
+    //    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
+    //    //    gse->setFilename("data/gestureSet20.dat");
+    //    //    gse->setOutputFilename("gestureset20PairsOutput83.csv");
+    //    //    gse->setTotalNrGest(20);
+    //    //    gse->setSkip(3);
+    //    //    gse->evaluatePairs();
+    //    vector<string> filenames;
+    //    filenames.push_back("data/makifree_2.dat");
+    //
+    //    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+    //    sde.setOutputFilename("results/free2.csv");
+    //    sde.setTotalNrGest(4);
+    //    sde.evaluate();
     writeToFile("this is thread 2");
 }
 
 void threadThree(void* arg) {
-//    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
-//    //    gse->setFilename("data/gestureSet20.dat");
-//    //    gse->setOutputFilename("gestureset20PairsOutput85.csv");
-//    //    gse->setTotalNrGest(20);
-//    //    gse->setSkip(5);
-//    //    gse->evaluatePairs();
-//    vector<string> filenames;
-//    filenames.push_back("data/makifree_3.dat");
-//
-//    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
-//    sde.setOutputFilename("results/free3.csv");
-//    sde.setTotalNrGest(4);
-//    sde.evaluate();
-    writeToFile("this is thread 1");
+    //    //    GestureSetEvaluation* gse = new GestureSetEvaluation();
+    //    //    gse->setFilename("data/gestureSet20.dat");
+    //    //    gse->setOutputFilename("gestureset20PairsOutput85.csv");
+    //    //    gse->setTotalNrGest(20);
+    //    //    gse->setSkip(5);
+    //    //    gse->evaluatePairs();
+    //    vector<string> filenames;
+    //    filenames.push_back("data/makifree_3.dat");
+    //
+    //    SyncDataEvaluation sde = SyncDataEvaluation(filenames);
+    //    sde.setOutputFilename("results/free3.csv");
+    //    sde.setTotalNrGest(4);
+    //    sde.evaluate();
+    writeToFile("this is thread 3");
 }
 
 /*
@@ -95,14 +100,20 @@ int main(int argc, char** argv) {
     //    SyncDataEvaluation* sde = new SyncDataEvaluation(filenames);
     //    sde->evaluate();
     //    sde->optimizeGVFParameters();
-
-    HANDLE t1 = (HANDLE) _beginthread(threadOne, 0, (void*) 1);
-    HANDLE t2 = (HANDLE) _beginthread(threadTwo, 0, (void*) 3);
-    HANDLE t3 = (HANDLE) _beginthread(threadThree, 0, (void*) 5);
-
-    WaitForSingleObject(t1, INFINITE);
-    WaitForSingleObject(t2, INFINITE);
-    WaitForSingleObject(t3, INFINITE);
-
+    int times = 3;
+    HANDLE handles[3*times];
+    for (int i = 0; i < times; i++) {
+        handles[(i/3)] = (HANDLE) _beginthread(threadOne, 0, (void*) (i/3));
+        handles[(i/3)+1] = (HANDLE) _beginthread(threadTwo, 0, (void*) ((i/3)+1));
+        handles[(i/3)+2] = (HANDLE) _beginthread(threadThree, 0, (void*) ((1/3)+2));
+    }
+    
+    for(int i = 0; i < (times * 3); i++){
+        WaitForSingleObject(handles[i], INFINITE);
+        printf("Thread %d done\n", i);
+//        WaitForSingleObject(t1, INFINITE);
+//        WaitForSingleObject(t2, INFINITE);
+//        WaitForSingleObject(t3, INFINITE);
+    }
     return 0;
 }
